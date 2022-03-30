eb90fa38954825851b33bd0af54783924bafaf06
Ralf <>
2022-03-30 12:46:41 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,7 +28,7 @@
                 <!-- DONUT CHART -->
                 <div class="card card-info">
                     <div class="card-header">
-                        <h3 class="card-title">COC Pie Chart</h3>
+                        <h3 class="card-title">COC Chart</h3>
                     </div>
                     <br>
                     <center>
