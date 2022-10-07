5bd2dd4478125ba1276b89db9e1c0653a9d4bf2f
Keno Renz Bacunawa <renzpalonpon@gmail.com>
2022-10-08 07:36:43 +0800

    db 003

--- config/database.php
+++ config/database.php
@@ -62,6 +62,21 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+        'old_db' => [
+            'driver' => env('DB_CONNECTION_OLD'),
+            'host' => env('DB_HOST_OLD', '127.0.0.1'),
+            'port' => env('DB_PORT_OLD', '3306'),
+            'database' => env('DB_DATABASE_OLD', 'forge'),
+            'username' => env('DB_USERNAME_OLD', 'forge'),
+            'password' => env('DB_PASSWORD_OLD', ''),
+            'unix_socket' => '',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
---
+++ scripts/db003.php
@@ -0,0 +1,3 @@
+<?php
+
+show_status($key + 1,count(1));
